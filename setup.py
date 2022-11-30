db9b9526a783a3725acacd506323952cb8fc27c5
rcarbajobenito <98690376+rcarbajobenito@users.noreply.github.com>
2022-11-30 14:18:19 +0100

    Create setup.py
    Create the setup.py to allow the distribution of the repository as a package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='snowflake',
+   version='1.0',
+   description='A module to draw snowflakes',
+   author='Rodrigo Carbajo',
+   packages=['snowflake'],
+)
